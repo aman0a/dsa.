c5418639b2ad598db716bc48adc85d02bc865b33
aman0a <151618379+aman0a@users.noreply.github.com>
2023-11-22 21:29:49 +0530

    Update 3 infix to postfix.cpp

--- 3 infix to postfix.cpp
+++ 3 infix to postfix.cpp
@@ -1,100 +1,107 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+#define MAX 100
+
 using namespace std;
-int chechpresidence(char a)
-{
-    if (a == '+' || a == '-')
-    {
-        return 1;
-    }
-    else if (a == '*' || a == '/')
-    {
-        return 2;
-    }
-    else if (a == '^')
-    {
-        return 3;
-    }
-    else
-    {
-        return -1;
-    }
-}
-class stack
-{
+
+class Stack {
+    int top;
+    char a[MAX];
 
 public:
-    int top = -1;
-    string st;
-    stack()
-    {}
-
-    void push(char exp)
-    {
-        top++;
-        st[top]=exp;
-    }
-
-    void pop()
-    {
-        st[top]='\0';
-        top--;
-    }
-    int empty()
-    {
-        if(st[0]=='\0')
-        return 0;
-        else 
-        return 1;
-    }
-    friend void infix_postfix(string );
+    Stack() { top = -1; }
+    bool push(char x);
+    char pop();
+    char peek();
+    bool isEmpty();
 };
 
-void infix_postfix(string str)
-{
-    stack s;
-    string result;
-    for (int i = 0; i < str.length(); i++)
-    {
-        char c = str[i];
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
-            result += c;
-        else if (c == '(')
-        {
-            s.push('(');
-        }
-        else if(c==')')
-        {
-            while(s.st[s.top]!='(')
-            {
-                result+=s.st[s.top];
-                s.pop();
-            }
-            s.pop();
-        }
-        else 
-        {
-            while(s.empty() &&(chechpresidence(str[i])<=chechpresidence(s.st[s.top])))
-            {
-                result+=s.st[s.top];
-                s.pop();
-            }
-            s.push(c);
-        }
+bool Stack::push(char x) {
+    if (top >= (MAX - 1)) {
+        cout << "Stack Overflow";
+        return false;
     }
-    while(s.empty())
-    {
-        result+=s.st[s.top];
-        s.pop();
+    else {
+        a[++top] = x;
+        return true;
     }
-    cout<<result<<endl;
 }
 
-int main()
+char Stack::pop() {
+    if (top < 0) {
+        cout << "Stack Underflow";
+        return 0;
+    }
+    else {
+        char x = a[top--];
+        return x;
+    }
+}
 
-{
-    string exp;
-    cout<<"enter the expression:"<<endl;
-    cin>>exp;
-    infix_postfix(exp);
+char Stack::peek() {
+    if (top < 0) {
+        cout << "Stack is Empty";
+        return 0;
+    }
+    else {
+        char x = a[top];
+        return x;
+    }
+}
+
+bool Stack::isEmpty() {
+    return (top < 0);
+}
+
+int isOperator(char ch) {
+    if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^')
+        return 1;
+    else
+        return 0;
+}
+
+int precedence(char ch) {
+    if (ch == '+' || ch == '-')
+        return 1;
+    else if (ch == '*' || ch == '/')
+        return 2;
+    else if (ch == '^')
+        return 3;
+    else
+        return 0;
+}
+
+string infixToPostfix(string infix) {
+    Stack stack;
+    string postfix;
+    for (int i = 0; i < infix.length(); i++) {
+        if ((infix[i] >= 'a' && infix[i] <= 'z') || (infix[i] >= 'A' && infix[i] <= 'Z')) {
+            postfix += infix[i];
+        }
+        else if (isOperator(infix[i]) == 1) {
+            if (stack.isEmpty())
+                stack.push(infix[i]);
+            else {
+                while (!stack.isEmpty() && precedence(infix[i]) <= precedence(stack.peek())) {
+                    postfix += stack.peek();
+                    stack.pop();
+                }
+                stack.push(infix[i]);
+            }
+        }
+    }
+    while (!stack.isEmpty()) {
+        postfix += stack.peek();
+        stack.pop();
+    }
+    return postfix;
+}
+
+int main() {
+    string infix_exp, postfix_exp;
+    cout << "Enter a Infix Expression : ";
+    cin >> infix_exp;
+    postfix_exp = infixToPostfix(infix_exp);
+    cout << "Postfix Expression: " << postfix_exp;
     return 0;
 }
